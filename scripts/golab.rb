46f94cd9bc0d5aece6242f33f4270cbd913b5846
dobby-zent <dobby.lee@zent.kr>
2024-11-01 14:11:36 +0900

    wip-a06c929c

---
+++ scripts/golab.rb
@@ -0,0 +1,11 @@
+class wip < Formula
+    desc "Wip"
+    homepage "https://github.com/zkfmapf123/Wip"
+    url "$RELEASE_URL"
+    sha256 "$RELEASE_CHECKSUM"
+  
+    def install
+      bin.install "wip"
+    end
+  end
+  
